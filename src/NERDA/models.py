f0243fff38dfdf39fc22934b8ea191c50d3783e3
Akseli Nurmi (TTL) <akseli.nurmi@helsinki.fi>
2022-05-10 16:33:39 +0300

    Probs to numpy in auroc

--- src/NERDA/models.py
+++ src/NERDA/models.py
@@ -376,7 +376,7 @@ class NERDA:
             tags_predicted, probs_predicted = self.predict(dataset.get('sentences'),
                                         return_tensors=True,
                                       **kwargs)
-            
+            print(probs_predicted)
             sm = torch.nn.Softmax(dim=1)
         else:
             tags_predicted = self.predict(dataset.get('sentences'), 
@@ -418,7 +418,7 @@ class NERDA:
             accuracy = accuracy_score(y_pred = flatten(tags_predicted), 
                                       y_true = flatten(dataset.get('tags')))
             if return_auroc:
-                auroc = compute_roc_auc_score(y_pred = sm(torch.tensor(flatten(probs_predicted))),
+                auroc = compute_roc_auc_score(y_pred = sm(torch.tensor(flatten(probs_predicted.numpy()))),
                                             y_true = dataset.get('tags'),
                                             labels = self.tag_scheme)
                 return {'f1':df, 'accuracy': accuracy, 'auroc': auroc}
